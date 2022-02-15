9ccdcba16440f658a518c1db7c44a6334135ac27
Dikshit Rishi Jain <ourledtv@gmail.com>
2022-02-15 12:04:32 +0530

    Added comments

--- clear-temp-files.bat
+++ clear-temp-files.bat
@@ -1,7 +1,9 @@
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 @echo OFF
+::This clears the Temp files
 del %TEMP%\*.* /f /s /q
+::Change C: to another letter if the 'Windows' folder is located in another drive
 del C:\Windows\Prefetch\*.* /f /s /q
 del C:\Windows\Temp\*.* /f /s /q
 defrag /C /O
