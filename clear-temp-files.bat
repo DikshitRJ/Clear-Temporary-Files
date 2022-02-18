21b70a54b5aa436435f885b0cc4b3ca86b0df450
Dikshit Rishi Jain <ourledtv@gmail.com>
2022-02-18 17:59:39 +0530

    Added comments

--- clear-temp-files.bat
+++ clear-temp-files.bat
@@ -1,13 +1,13 @@
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 @echo OFF
-::The first three lines clear the temporary files
+ECHO Deleting temp files
 del %TEMP%\*.* /f /s /q
 del %systemdrive%\Windows\Prefetch\*.* /f /s /q
 del %systemdrive%\Windows\Temp\*.* /f /s /q
-::This defragments and optimizes all drives
+ECHO Defragmenting all drives
 defrag /C /O
-::This clears the Recycle Bin
+ECHO  Clearing recycle bin
 rd /s /q %systemdrive%\$Recycle.bin
-::This runs the Disk Cleanup utility
+ECHO Bin cleared. Running Disk Cleanup
 cleanmgr.exe /d%systemdrive%
